3214191b8a7b8642f94415dda9d5ed35bec7b7e4
nittyanandr17 <64355215+nittyanandr17@users.noreply.github.com>
2025-06-13 12:28:52 +0530

    Add files via upload

---
+++ Task 1 Perform Basic Mathematical Operations.py
@@ -0,0 +1,13 @@
+print("please provide the numbers as follows:")
+a=input('Num1: ')
+b=input('Num2: ')
+a=float(a)
+b=float(b)
+A= a+b
+S= a-b
+M= a*b
+D= a/b
+print('Addition: ',A)
+print('Subtraction: ',S)
+print('Multiplication: ',M)
+print('Division: ',D)
---
+++ Task2 Create a personalised greeting.py
@@ -0,0 +1,4 @@
+# Creating a personalised greeting
+a=input('Enter the fist name: ')
+b= input('Enter the last name: ')
+print("Hello,",a ,b,"Welcome to the Python program.")
